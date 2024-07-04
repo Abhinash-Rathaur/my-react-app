9ecbedb6f47728216d495b3e183b703a0fade203
Abhinash-Rathaur <rathaur.abhinash82@gmail.com>
2024-07-04 15:14:13 +0530

    Updates

---
+++ precache-manifest.8227d0341bdd97c45077e2b188e20e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eca16745b80ae8f7127f9b0ef7b5c81",
+    "url": "/MY-APP/index.html"
+  },
+  {
+    "revision": "ce7312a06eccb8d82c56",
+    "url": "/MY-APP/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "20f6513de2d0ac94acdc",
+    "url": "/MY-APP/static/js/2.2b166d89.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/MY-APP/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "ce7312a06eccb8d82c56",
+    "url": "/MY-APP/static/js/main.096fc7e0.chunk.js"
+  },
+  {
+    "revision": "51febd20d0d8366623ce",
+    "url": "/MY-APP/static/js/runtime~main.b495d531.js"
+  }
+]);
